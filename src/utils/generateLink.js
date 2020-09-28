76259e91580d9b88d9d9d2f9f9301f4fedc91bba
Tarmo <tarmo888@hotmail.com>
2020-09-28 13:16:55 +0300

    force to use single address where necessary

--- src/utils/generateLink.js
+++ src/utils/generateLink.js
@@ -1,17 +1,11 @@
 import config from "../config";
 import { encodeData } from "./encodeData";
 
-export const generateLink = (amount, data, address, AA, asset) => {
+export const generateLink = (amount, data, address, AA, asset, is_single) => {
   const sData = encodeData(data);
-  return address
-    ? `obyte${
+  return `obyte${
         config.TESTNET ? "-tn" : ""
       }:${AA}?amount=${amount}&base64data=${encodeURIComponent(
         sData
-      )}&from_address=${address}&asset=${asset || "base"}`
-    : `obyte${
-        config.TESTNET ? "-tn" : ""
-      }:${AA}?amount=${amount}&base64data=${encodeURIComponent(sData)}&asset=${
-        asset || "base"
-      }`;
+      )}&from_address=${address || ''}&single_address=${is_single ? '1' : '0'}&asset=${asset || "base"}`;
 };
