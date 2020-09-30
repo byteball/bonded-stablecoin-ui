2e065c1231acb9cad3c9ba9e2e37ac4b0d0b06cd
Tarmo <tarmo888@hotmail.com>
2020-09-30 21:42:11 +0300

    eslint fix

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -20,7 +20,7 @@ export const MainMenu = ({ mode, pathname, onClose }) => {
           to={`/trade${address ? "/" + address : ""}`}
           activeClassName="selected"
         >
-          📈 Trade
+          <span role="img" aria-label="Chart">📈</span> Trade
         </NavLink>
       </Menu.Item>
       <Menu.Item key="/create">
