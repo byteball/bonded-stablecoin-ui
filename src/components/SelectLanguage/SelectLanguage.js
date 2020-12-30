b3621106cd5285d8292d54a8b688159728a2c0d6
Taump <taump@yandex.ru>
2020-12-31 01:11:15 +0700

    add da flag

--- src/components/SelectLanguage/SelectLanguage.js
+++ src/components/SelectLanguage/SelectLanguage.js
@@ -9,6 +9,7 @@ import ru from "./flags/ru.svg"
 import usa from "./flags/usa.svg"
 import es from "./flags/es.svg"
 import cn from "./flags/cn.svg"
+import da from "./flags/da.svg"
 
 export const langs = [
   {
@@ -29,7 +30,7 @@ export const langs = [
   },
   {
     name: "da",
-    flag: ru
+    flag: da
   },
 ];
 
