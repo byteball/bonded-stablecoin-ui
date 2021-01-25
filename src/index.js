596cca1afb439f403ed268b4af37cdd863e41df2
Taump <taump@yandex.ru>
2021-01-25 22:56:00 +0700

    add craco

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import ReactGA from "react-ga";
 
 import { PersistGate } from "redux-persist/integration/react";
-import "antd/dist/antd.css";
+import "antd/dist/antd.less";
 import "./index.css";
 import AppRouter from "./AppRouter";
 import configureStore from "./store";
