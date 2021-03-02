3c82db38b02d00de3cb8ed61378428879479c2f4
Taump <taump@yandex.ru>
2021-03-03 01:57:27 +0700

    fix

--- src/store/actions/active/addTransaction.js
+++ src/store/actions/active/addTransaction.js
@@ -2,54 +2,55 @@ import { ADD_NEW_TRANSACTION, ADD_STABLE_TRANSACTION, UPDATE_TRANSACTION } from 
 
 const types = ["curve", "deposit", "governance"];
 
-export const addTransaction = ({isStable, type, AAReqOrRes}) => async (dispatch, getState, socket) => {
-  if(!AAReqOrRes) return null
-  if(types.includes(type)){
-    if (isStable) {
-    const store = getState();
-    
-    if(AAReqOrRes.trigger_unit in store.active.transactions[type]){
-      dispatch({
-        type: UPDATE_TRANSACTION,
-        payload: {
-          unit: AAReqOrRes.trigger_unit,
-          objResponseUnit: AAReqOrRes.objResponseUnit,
-          type
-        }
-      })
-    } else {
-      const {trigger_unit, trigger_address} = AAReqOrRes;
-      const { joint: { unit: trigger_unit_info } } = await socket.api.getJoint(trigger_unit);
+export const addTransaction = ({ isStable, type, unit, response }) => async (dispatch, getState, socket) => {
+  if (!response && !unit) return null
 
-      dispatch({
-        type: ADD_STABLE_TRANSACTION,
-        payload: {
-          ...trigger_unit_info,
-          isStable: true,
-          objResponseUnit: AAReqOrRes.objResponseUnit,
-          bounced: AAReqOrRes.bounced,
-          trigger_address,
-        },
-        meta: {
-          type,
-          unit: trigger_unit
-        }
-      })
-    }
+  if (types.includes(type)) {
+    if (isStable) {
+      const store = getState();
+
+      if (response.trigger_unit in store.active.transactions[type]) {
+        dispatch({
+          type: UPDATE_TRANSACTION,
+          payload: {
+            unit: response.trigger_unit,
+            objResponseUnit: response.objResponseUnit,
+            type
+          }
+        })
+      } else {
+        const { trigger_unit, trigger_address } = response;
+        const { joint: { unit: trigger_unit_info } } = await socket.api.getJoint(trigger_unit);
+
+        dispatch({
+          type: ADD_STABLE_TRANSACTION,
+          payload: {
+            ...trigger_unit_info,
+            isStable: true,
+            objResponseUnit: response.objResponseUnit,
+            bounced: response.bounced,
+            trigger_address,
+          },
+          meta: {
+            type,
+            unit: trigger_unit
+          }
+        })
+      }
 
     } else {
       const payload = {
-        trigger_address: AAReqOrRes.authors[0]?.address,
-        ...AAReqOrRes,
+        trigger_address: unit.authors[0]?.address,
+        ...unit,
         bounced: 0,
-        trigger_unit: AAReqOrRes.unit,
+        trigger_unit: unit.unit,
         objResponseUnit: {},
         isStable
       }
       dispatch({
         type: ADD_NEW_TRANSACTION,
         payload,
-        meta: {type, unit: AAReqOrRes.unit}
+        meta: { type, unit: unit.unit }
       });
     }
   } else {
