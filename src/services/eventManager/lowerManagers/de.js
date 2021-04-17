0bc367e66767ca32b6a339390aa02de2d7dce7ff
Taump <taump@yandex.ru>
2021-04-17 19:20:04 +0700

    fix notification

--- src/services/eventManager/lowerManagers/de.js
+++ src/services/eventManager/lowerManagers/de.js
@@ -36,24 +36,25 @@ export const deEventManager = ({
         }
       }
       if (action === "buy") {
+        const amount = messages.find((msg) => msg.app === "payment" && !("asset" in msg.payload))?.payload?.outputs?.find((output) => output.address === decision_engine_aa).amount / 10 ** reserve_asset_decimals;
         if (isAuthor) {
           openNotification(
-            i18n.t("notification.de.buy.req_author", "You have sent a request to buy {{symbol}}", { symbol: symbol4 || "T_SF" })
+            i18n.t("notification.de.buy.req_author", "You have sent a request to buy {{symbol}} for {{amount}} GBYTE", { symbol: symbol4 || "T_SF", amount })
           );
         } else {
           openNotification(
-            i18n.t("notification.de.buy.req", "Another user sent a request to buy {{symbol}}", { symbol: symbol4 || "T_SF" })
+            i18n.t("notification.de.buy.req", "Another user sent a request to buy {{symbol}} for {{amount}} GBYTE", { symbol: symbol4 || "T_SF", amount })
           );
         }
       } else if (action === "redeem") {
-        const count = messages.find((msg) => msg.app === "payment" && ("asset" in msg.payload))?.payload?.outputs?.find((output) => output.address === decision_engine_aa).amount / 10 ** reserve_asset_decimals;
+        const amount = messages.find((msg) => msg.app === "payment" && ("asset" in msg.payload))?.payload?.outputs?.find((output) => output.address === decision_engine_aa).amount / 10 ** reserve_asset_decimals;
         if (isAuthor) {
           openNotification(
-            i18n.t("notification.de.sell.req_author", "You have sent a request to redeem {{count}} {{symbol}}", { symbol: symbol4 || "T_SF", count })
+            i18n.t("notification.de.sell.req_author", "You have sent a request to redeem {{amount}} {{symbol}}", { symbol: symbol4 || "T_SF", amount })
           );
         } else {
           openNotification(
-            i18n.t("notification.de.sell.req", "Another user sent a request to redeem {{count}} {{symbol}}", { symbol: symbol4 || "T_SF", count })
+            i18n.t("notification.de.sell.req", "Another user sent a request to redeem {{amount}} {{symbol}}", { symbol: symbol4 || "T_SF", amount })
           );
         }
       }
