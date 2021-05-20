e13af4e1e76fc69da87d188866133a7037fb98c3
Taump <taump@yandex.ru>
2021-05-20 17:36:12 +0700

    fix

--- src/store/actions/data/getData.js
+++ src/store/actions/data/getData.js
@@ -1,6 +1,16 @@
 import { LOAD_SNAPSHOT_SUCCESS } from "store/types";
+import { changeActive } from "../active/changeActive";
 
-export const getData = (state, balances) =>({
-  type: LOAD_SNAPSHOT_SUCCESS,
-  payload: {state, balances}
-});
+export const getData = (state, balances) => async (dispatch, getState) => {
+  const store = getState();
+
+  dispatch({
+    type: LOAD_SNAPSHOT_SUCCESS,
+    payload: { state, balances }
+  })
+
+  if (store.active.address) {
+    dispatch(changeActive(store.active.address))
+  }
+
+};
